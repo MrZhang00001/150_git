2cad183f45a57d07d944d3c73cca1b46cf9fc03c
test <51193956+MrZhang00001@users.noreply.github.com>
2019-06-02 10:31:36 +0800

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,6 +10,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
         System.out.println("第一次提交测试" + "test===");
         System.out.println("第二次提交测试" + "test2===");
+        System.out.println("第三次提交测试" + "test3===");
     }
 
 }
