4cace886b5c82754d9e60049b95c53b5726112ff
zm <1487401615@qq.com>
2019-06-02 10:18:54 +0800

    v1.1

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("第一次提交测试" + "test===");
+        System.out.println("第二次提交测试" + "test2===");
     }
 
 }
